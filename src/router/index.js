9a2517c246b08d970c877b3a45ea685b1ca2d1e8
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-03-27 15:24:04 +0500

    template pushed

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+const routes = [
+    {
+        path: '/',
+        component: () => import('@/views/HomeView.vue')
+    },
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+})
+
+export default router;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <p class="text-3xl">Home View</p>
+</template>
+
+<style scoped>
+
+</style>
